8360c90d48ba5438399b0cd6d73ac4f90f282ae1
Sebas3004tian <sebas3004tian@gmail.com>
2024-11-04 09:10:34 -0500

    feat: View user information (seller only)

--- src/components/SellerNavbar.tsx
+++ src/components/SellerNavbar.tsx
@@ -1,11 +1,42 @@
 "use client";
 
-import {useRouter} from "next/navigation";
+import { useState, useEffect } from "react";
+import { Mail, MapPin } from "lucide-react";
+import { useRouter } from "next/navigation";
+import UserVector from '../components/svg/User.svg';
+import Image from 'next/image';
+import Cookies from 'js-cookie';
 
 export default function SellerNavbar() {
     const router = useRouter();
+    const [isProfileMenuOpen, setProfileMenuOpen] = useState(false);
+    const [currentUser, setCurrentUser] = useState({ fullName: "", email: "", address: "" });
+
+    useEffect(() => {
+        const cookieValue = Cookies.get('currentUser');
+
+        if (cookieValue) {
+            const user = JSON.parse(cookieValue);
+            setCurrentUser({
+                fullName: user.fullName,
+                email: user.email,
+                address: user.address,
+            });
+        }
+    }, []);
+
+    const toggleProfileMenu = () => {
+        setProfileMenuOpen(!isProfileMenuOpen);
+    };
+
+    const handleLogout = () => {
+        Cookies.remove('token');
+        Cookies.remove('currentUser');
+        window.location.href = "/login";
+    };
+
     return (
-        <nav className="bg-gray-900 text-white p-4">
+        <nav className="bg-gray-900 text-white p-5 relative">
             <div className="container mx-auto flex justify-between items-center">
                 <h1 className="text-2xl">Stock-Vendedor</h1>
                 <div>
@@ -13,8 +44,52 @@ export default function SellerNavbar() {
                     <button onClick={() => router.push('/seller/orders')} className="mr-8">Ventas</button>
                     <button onClick={() => router.push('/seller/categories')}>Categorías y subcategorías</button>
                 </div>
-                <button className="bg-gray-700 p-2 rounded-full">Perfil</button>
+                <button onClick={toggleProfileMenu} className="bg-[#D9D9D9] p-2 rounded-full">
+                    <Image
+                        priority
+                        src={UserVector}
+                        alt=""
+                        width={25}
+                        height={25}
+                    />
+                </button>
             </div>
+            {isProfileMenuOpen && (
+                <div className="absolute top-20 right-0 bg-gray-800 text-white w-376 p-4 rounded-bl-lg shadow-lg z-10">
+                    <ul>
+                        <div className="w-full max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
+                            <div className="flex flex-col items-center p-6">
+                                <div className="w-24 h-24 rounded-full overflow-hidden bg-gray-200 mb-4">
+                                    <Image
+                                        priority
+                                        src={UserVector}
+                                        alt=""
+                                        width={100}
+                                        height={100}
+                                    />
+                                </div>
+                                <h2 className="text-2xl font-bold text-gray-800 mb-2">{currentUser.fullName}</h2>
+                                <div className="flex flex-col items-center space-y-2 text-sm text-gray-600">
+                                    <div className="flex items-center">
+                                        <MapPin className="w-4 h-4 mr-2 text-gray-400" />
+                                        <span>{currentUser.address}</span>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <Mail className="w-4 h-4 mr-2 text-gray-400" />
+                                        <span>{currentUser.email}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <li className="border-b border-gray-700 py-2 cursor-pointer" onClick={() => router.push('/profile/edit')}>
+                            Editar tu información
+                        </li>
+                    </ul>
+                    <button className="w-full mt-4 text-center py-2 border-t border-gray-700" onClick={handleLogout}>
+                        Cerrar Sesión
+                    </button>
+                </div>
+            )}
         </nav>
     );
 }
